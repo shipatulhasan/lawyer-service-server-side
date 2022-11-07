f052068de8dac59efeaed9dd22bfc4b0d2c8b112
shipatulhasan <shipatulhasan328@gmail.com>
2022-11-07 23:16:00 +0600

    initialization

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+
+    res.send('Hello bubu from node')
+})
+
+app.listen(port,()=>{
+    console.log(`listing from ${port}`)
+})
