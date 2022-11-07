51eda667b25b25a161050cac37b7653f0a8bbb86
shipatulhasan <shipatulhasan328@gmail.com>
2022-11-08 00:06:57 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
@@ -11,6 +13,31 @@ app.get('/', (req,res)=>{
     res.send('Hello bubu from node')
 })
 
+
+// mongdb
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0vh6mry.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
 app.listen(port,()=>{
+
+    client.connect(err => {
+        // const collection = client.db("test").collection("devices");
+        console.log('connected to mongodb')
+        // perform actions on the collection object
+        client.close();
+      });
+
+
     console.log(`listing from ${port}`)
 })
