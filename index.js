e27132ea3ed8ea58f4c8ca96a9cad2959fceec70
shipatulhasan <shipatulhasan328@gmail.com>
2022-11-09 12:12:17 +0600

    create appi for single service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 var jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 require('dotenv').config()
@@ -39,6 +39,17 @@ const run = async()=>{
             const cursor = servicesCollection.find(query).sort({_id:-1})
 
             const result = await cursor.limit(limit).toArray()
+            res.send(result)
+        })
+
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id
+
+            const query = {_id:ObjectId(id)}
+
+            const result = await servicesCollection.findOne(query)
+
+            console.log(result)
             res.send(result)
         })
 
